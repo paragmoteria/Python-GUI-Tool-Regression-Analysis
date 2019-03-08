83fd8593ae8c6d60b0c0e8cfd7699e527a241280
Parag Moteria <45424850+paragmoteria@users.noreply.github.com>
2019-03-08 07:47:48 +0530

    Update GUI_Main.py

--- GUI_Main.py
+++ GUI_Main.py
@@ -1,3 +1,9 @@
+"""
+@author: Parag Moteria, Machine Learning Consultant
+
+email-id : paragmoteria@gmail.com
+"""
+
 import tkinter as tk
 from tkinter import scrolledtext
 from tkinter import ttk
@@ -7,100 +13,115 @@ from tkinter.filedialog import askopenfilename
 import io
 import os
 import pandas as pd
-import numpy as np
 
-win=tk.Tk()
-win.title("Python GUI (Data Science) - Regression Model, k-Mean & Neural Network")
-#win.geometry("550x550+10+10")
+win = tk.Tk()
+win.title("Python GUI (Data Science) - Regression Model")
 win.overrideredirect(False)
-win.geometry("{0}x{1}+10+10".format(win.winfo_screenwidth()-100, win.winfo_screenheight()-100))
-win.resizable(0,0)
-
-#res_label = ttk.Label(win , text = "Result Text")
-#res_label.grid(row=2,column=1)
-
+win.geometry("{0}x{1}+10+10".format(win.winfo_screenwidth() - 100,
+             win.winfo_screenheight()-100))
+win.resizable(0, 0)
 scr = scrolledtext.ScrolledText(win, width=150, height=30, wrap=tk.WORD)
 scr.grid(column=0, columnspan=3)
 
-# Load Data
+
 def readBtn():
+    '''
+    Read data csv file
+    '''
     global df
-    Tk().withdraw() 
+    Tk().withdraw()
     filename = askopenfilename()
     df = pd.read_csv(filename)
-    scr.insert(tk.INSERT , '\n Data File ... \n\n')
+    scr.insert(tk.INSERT, '\n Data File ... \n\n')
     pd.set_option('display.max_columns', 500)
     df1 = df.drop(df.columns[0], axis=1)
-    scr.insert(tk.INSERT , df1)
+    scr.insert(tk.INSERT, df1)
 
-# is Missing Value?
+
 def missValue():
-    scr.insert( tk.INSERT , '\n\n Missing Value Anaysis ... \n\n')
+    '''
+    Identify Missing Values
+    '''
+    scr.insert(tk.INSERT, '\n\n Missing Value Anaysis ... \n\n')
     pd.set_option('display.max_columns', 500)
     buf = io.StringIO()
     df.info(buf=buf)
     s = buf.getvalue()
-    scr.insert(tk.INSERT , s)
+    scr.insert(tk.INSERT, s)
 
-## Complete Case with Descriptive Statistics
+
 def completeCase():
-    scr.insert( tk.INSERT , '\n\n Complete Case only ... \n\n')
+    '''
+    It is not always feasible.
+    Drop missing values
+    '''
+    scr.insert(tk.INSERT, '\n\n Complete Case only ... \n\n')
     df1 = df.drop(df.columns[0], axis=1)
     df1 = df1.dropna()
-    path = os.getcwd() + "\\datasets\\completeCases.csv"
+    if not os.path.isdir("imputedData"):
+        os.mkdir("imputedData")
+    path = os.getcwd() + "\\imputedData\\completeCases.csv"
     df1.to_csv(path)
-    scr.insert(tk.INSERT , df1.describe())
+    scr.insert(tk.INSERT, df1.describe())
 
-## Data Pre-Processing with Descriptive Statistics (using Median)
+
 def cleanData():
+    '''
+    Data Pre-Processing with Descriptive Statistics (using Median)
+    It is suitable for numeric data
+    '''
     df1 = df.drop(df.columns[0], axis=1)
-    df1.fillna(df1.median() , inplace=True)
-    path = os.getcwd() + "\\datasets\\imputeMissingVal.csv"
+    df1.fillna(df1.median(), inplace=True)
+    if not os.path.isdir("imputedData"):
+        os.mkdir("imputedData")
+    path = os.getcwd() + "\\imputedData\\imputeMissingVal.csv"
     df1.to_csv(path)
-    scr.insert( tk.INSERT , '\n\n Cleaning Data by Imputing Missing Value using Median ... \n\n')
-    scr.insert(tk.INSERT , df1.describe())
+    scr.insert(tk.INSERT, '\n\n Cleaning Data by Imputing Missing Value \
+               using Median ... \n\n')
+    scr.insert(tk.INSERT, df1.describe())
 
-## Explore Missing Value using Correlation
+
 def summaryInfo():
+    '''
+    Explore Missing Value using Correlation
+    '''
     x = df.drop(df.columns[0], axis=1)
-    x.fillna(1 , inplace=True)
-    x[x!=1] = 0
-    scr.insert( tk.INSERT , '\n\n Explore Missing Value using Correlation \n\n')
-    scr.insert(tk.INSERT , x.corr())
+    x.fillna(1, inplace=True)
+    x[x != 1] = 0
+    scr.insert(tk.INSERT, '\n\n Explore Missing Value using Correlation \n\n')
+    scr.insert(tk.INSERT, x.corr())
 
-## Multiple Regression Analysis
+
 def regAna():
+    '''
+    Multiple Regression Analysis
+    '''
     from stat_study import create_window
     create_window()
-    
-read_btn = ttk.Button(win , text = "Read Data - CSV File" , command = readBtn)
-read_btn.grid(row=1,column=0)
 
-miss_btn = ttk.Button(win , text = "is Missing Value?" , command = missValue)
-miss_btn.grid(row=3,column=0)
 
-complete_btn = ttk.Button(win , text = "Complete Cases - Descriptive Statistics" , command = completeCase)
-complete_btn.grid(row=1,column=1)
+read_btn = ttk.Button(win, text="Read Data - CSV File", command=readBtn)
+read_btn.grid(row=1, column=0)
 
-cleanStr_btn = ttk.Button(win , text = "Data Pre-Preparation - Impute Missing Value (Summary)" , command = cleanData)
-cleanStr_btn.grid(row=3,column=1)
+miss_btn = ttk.Button(win, text="is Missing Value?", command=missValue)
+miss_btn.grid(row=3, column=0)
 
-missVal_corr_btn = ttk.Button(win , text = "Explore Missing Value using Correlation" , command = summaryInfo)
-missVal_corr_btn.grid(row=1,column=2)
+complete_btn = ttk.Button(win, text="Complete Cases - Descriptive Statistics",
+                          command=completeCase)
+complete_btn.grid(row=1, column=1)
 
-exit_btn = ttk.Button(win , text = "Exit" , command=win.destroy)
-exit_btn.grid(row=3,column=2)
+cleanStr_btn = ttk.Button(win, text="Data Pre-Preparation - Impute Missing",
+                          command=cleanData)
+cleanStr_btn.grid(row=3, column=1)
 
-reg_btn = ttk.Button(win , text = "Regression Model" , command = regAna)
-reg_btn.grid(row=11,column=0,padx=50,pady=50)
+missVal_corr_btn = ttk.Button(win, text="Missing Value using Correlation",
+                              command=summaryInfo)
+missVal_corr_btn.grid(row=1, column=2)
 
-#kmean_btn = ttk.Button(win , text = "Cluster Analysis - (k-Mean)" , command = regAna)
-#kmean_btn.grid(row=11,column=1)
+exit_btn = ttk.Button(win, text="Exit", command=win.destroy)
+exit_btn.grid(row=3, column=2)
 
-#neural_btn = ttk.Button(win , text = "Classification Model - (Neural Network)" , command = regAna)
-#neural_btn.grid(row=11,column=2)
-
-#scr = scrolledtext.ScrolledText(win, width=60, height=20, wrap=tk.WORD)
-#scr.grid(column=0, columnspan=3)
+reg_btn = ttk.Button(win, text="Regression Model", command=regAna)
+reg_btn.grid(row=11, column=0, padx=50, pady=50)
 
 win.mainloop()
